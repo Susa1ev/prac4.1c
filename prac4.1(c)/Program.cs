bdf7b89ece96f758f0de4c3e467a2202c3121a9f
Susa1ev <susalytt@gmail.com>
2021-11-30 22:57:02 +0300

    Добавьте файлы проекта.

---
+++ prac4.1(c)/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace prac4._1_c_
+{
+    class prac
+    {
+        int a, b;
+        public prac()
+        {
+            reTry:
+            Console.WriteLine("Введите два целочисленных значения:");
+            if( Int32.TryParse(Console.ReadLine(), out a) &&
+                Int32.TryParse(Console.ReadLine(), out b))
+            {
+                Console.WriteLine(nod(Math.Abs(a), Math.Abs(b)));
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то, попробуйте ещё раз");
+                goto reTry;
+            }
+        }
+        int nod(int a,int b)
+        {
+            if(a == b) { return a; }
+            else if(a > b)
+            {
+                return nod(a - b, b);
+            }
+            else
+            {
+                return nod(a, b-a);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
